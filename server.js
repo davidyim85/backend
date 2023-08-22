bf71df653328419b89dd785901e60de7c3738dd6
yimd85 <yimd85@gmail.com>
2023-08-21 22:08:59 -0400

    aas

--- server.js
+++ server.js
@@ -226,7 +226,7 @@ app.post("/signup", async (req, res) => {
         // secure = only send cookie over https
         secure: false,
         // sameSite = only send cookie if the request is coming from the same origin
-        sameSite: "lax", // "strict" | "lax" | "none" (secure must be true)
+        sameSite: "none", // "strict" | "lax" | "none" (secure must be true)
         // maxAge = how long the cookie is valid for in milliseconds
         maxAge: 3600000, // 1 hour
       });
